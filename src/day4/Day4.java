8456574f3f1f202e11b98fca688c9241cb3dda74
Daniel Brand <branddan@stud.sbg.ac.at>
2017-12-04 19:20:14 +0100

    add day4 part2

--- src/day4/Day4.java
+++ src/day4/Day4.java
@@ -3,7 +3,9 @@ package day4;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Day4 {
@@ -11,7 +13,7 @@ public class Day4 {
   public static void main(String[] args) {
     long sum = 0;
     try (Stream<String> fileStream = Files.lines(Paths.get("resources/day4.txt"))) {
-      sum = fileStream.mapToInt(Day4::isValidString).sum();
+      sum = fileStream.mapToInt(Day4::checkForDuplicateWords).sum();
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -20,7 +22,7 @@ public class Day4 {
   }
 
   //Part 1
-  private static int isValidString(String s) {
+  private static int checkForDuplicateWords(String s) {
     String[] line = s.split(" ");
     long count = Arrays.stream(line).distinct().count();
     if (count == line.length) {
@@ -30,4 +32,22 @@ public class Day4 {
     }
   }
 
+  //part 2
+  private static int checkForDuplicateChars(String s) {
+    List<String> words = Arrays.asList(s.split(" "));
+    List<List<String>> splitCharsPerWord = new ArrayList<>();
+    List<String> sortedList;
+
+    for (String word : words) {
+      sortedList = Arrays.asList(word.split(""));
+      sortedList.sort(String::compareTo);
+      splitCharsPerWord.add(sortedList);
+    }
+
+    if (splitCharsPerWord.stream().distinct().count() == (long) splitCharsPerWord.size()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
