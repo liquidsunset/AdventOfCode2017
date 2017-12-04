4d036493376509c7b8cd47cf6032f56c8cbf1f44
Daniel Brand <branddan@stud.sbg.ac.at>
2017-12-04 22:39:08 +0100

    add day3 part2

--- src/day3/Day3.java
+++ src/day3/Day3.java
@@ -1,5 +1,6 @@
 package day3;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Day3 {
@@ -7,7 +8,7 @@ public class Day3 {
   private static final int INPUT = 368078;
 
   public static void main(String[] args) {
-    System.out.println(getManhattanDistance());
+    System.out.println(getFirstSum());
   }
 
   //Part 1
@@ -48,6 +49,98 @@ public class Day3 {
     } else {
       return matrixSquaresList.size() - middleElement + squareCountPerBlock;
     }
+  }
+
+  enum Moves {
+    UP, DOWN, LEFT, RIGHT, INIT
+  }
+
+  //part 2
+  private static int getFirstSum() {
+
+    int[][] matrix = new int[51][51];
+    Moves move = Moves.INIT;
+    int actualSquareSum = 0;
+    int i = 25, j = 25;
+    matrix[i][j] = 1;
+    printMatrix(matrix);
+
+    int ring = 1;
+    int filledSquares = 0;
+    int numberSquares = ring * 8;
+    int numberPerRow = numberSquares / 4;
+    while (INPUT > actualSquareSum) {
+      filledSquares++;
+      switch (move) {
+        case INIT:
+          j++;
+          break;
+        case UP:
+          i--;
+          break;
+        case RIGHT:
+          j--;
+          break;
+        case DOWN:
+          i++;
+          break;
+        case LEFT:
+          j++;
+          break;
+      }
+
+      if (Moves.INIT.equals(move)) {
+        move = Moves.UP;
+      }
+
+      actualSquareSum = sumUpNeighbours(matrix, i, j);
+      matrix[i][j] = actualSquareSum;
+
+      if (filledSquares % numberPerRow == 0) {
+        int side = filledSquares / numberPerRow;
+        switch (side) {
+          case 3:
+            move = Moves.LEFT;
+            break;
+          case 2:
+            move = Moves.DOWN;
+            break;
+          case 1:
+            move = Moves.RIGHT;
+            break;
+          default:
+            move = Moves.INIT;
+
+        }
+      }
+
+      if (filledSquares == numberSquares) {
+        ring++;
+        filledSquares = 0;
+        numberSquares = ring * 8;
+        numberPerRow = numberSquares / 4;
+      }
+
+    }
+
+    return actualSquareSum;
+  }
+
+  private static int sumUpNeighbours(int[][] matrix, int i, int j) {
+    return matrix[i][j + 1] + matrix[i - 1][j + 1] + matrix[i - 1][j] + matrix[i - 1][j - 1]
+        + matrix[i][j - 1] + matrix[i + 1][j - 1] + matrix[i + 1][j] + matrix[i + 1][j + 1];
+  }
+
+  private static void printMatrix(int[][] matrix) {
+    Arrays.stream(matrix)
+        .forEach(
+            (row) -> {
+              System.out.print("[");
+              Arrays.stream(row)
+                  .forEach((el) -> System.out.print(" " + el + " "));
+              System.out.println("]");
+            }
+        );
   }
 
 }
