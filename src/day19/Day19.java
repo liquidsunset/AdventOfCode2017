dc2f866ec91a8a52bebfaf2c5b4e3e46d67d92ca
Daniel Brand <branddan@stud.sbg.ac.at>
2018-01-13 23:30:10 +0100

    add day 19 part 1 and part 2

---
+++ src/day19/Day19.java
@@ -0,0 +1,112 @@
+package day19;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day19 {
+
+  public static void main(String[] args) {
+    try (Stream<String> fileStream = Files.lines(Paths.get("resources/day19.txt"))) {
+      List<List<String>> map = new ArrayList<>();
+      fileStream.forEach(line -> {
+        List<String> splitLine = Arrays.asList(line.split(""));
+        map.add(splitLine);
+      });
+
+      Point point = new Point(map.get(0).indexOf("|"), 0);
+      StringBuilder letters = new StringBuilder();
+      int steps = 0;
+
+      Direction direction = Direction.SOUTH;
+      while (true) {
+        char move = map.get(point.y).get(point.x).charAt(0);
+        if (move == ' ') {
+          break;
+        } else if (move == '+') {
+          direction = updateDirection(map, point, direction);
+        } else if (Character.isLetter(move)) {
+          letters.append(move);
+        }
+
+        assert direction != null;
+        updatePoint(point, direction);
+
+        steps++;
+      }
+
+      System.out.println("Part 1: " + letters);
+      System.out.println("Part 2: " + steps);
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static Direction updateDirection(List<List<String>> map, Point point,
+      Direction direction) {
+    int row = point.y;
+    int column = point.x;
+    switch (direction) {
+      case NORTH:
+      case SOUTH:
+        char move = map.get(row).get(column - 1).charAt(0);
+        if (move == ' ') {
+          return Direction.EAST;
+        } else {
+          return Direction.WEST;
+        }
+      case EAST:
+      case WEST:
+        List<String> mapRow = map.get(row - 1);
+        if (mapRow.size() > column && mapRow.get(column).charAt(0) != ' ') {
+          return Direction.NORTH;
+        } else {
+          return Direction.SOUTH;
+        }
+    }
+    return null;
+  }
+
+  private static void updatePoint(Point point, Direction direction) {
+    switch (direction) {
+      case NORTH:
+        point.addPoint(new Point(0, -1));
+        break;
+      case EAST:
+        point.addPoint(new Point(1, 0));
+        break;
+      case SOUTH:
+        point.addPoint(new Point(0, 1));
+        break;
+      case WEST:
+        point.addPoint(new Point(-1, 0));
+        break;
+    }
+  }
+
+  enum Direction {
+    NORTH, EAST, SOUTH, WEST
+  }
+
+  private static class Point {
+
+    int x;
+    int y;
+
+    Point(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    void addPoint(Point move) {
+      this.x += move.x;
+      this.y += move.y;
+    }
+  }
+
+}
